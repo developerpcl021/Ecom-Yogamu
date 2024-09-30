2f6729dbf041c64be1d35f5f081fef13a3e25220
dev021 <nitinjain.work1991@gamil.com>
2024-09-30 17:19:13 +0530

    sada

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+const header = document.getElementById("header");
+const Desktopheader = document.getElementById("desktop_header");
+const tooglebutton = document.getElementById("show_header");
+const Hidebutton = document.getElementById("hide_header");
+const HeroSection = document.getElementById("hero_section");
+
+// Toggle
+let toogle = false;
+
+if (window.innerWidth > 767) {
+  header.style.left = "-768px";
+  document.body.style.overflow = "unset";
+}
+
+tooglebutton.addEventListener("click", function () {
+  if (toogle === false) {
+    showSideBar();
+  } else {
+    hideSideBar();
+  }
+});
+
+Hidebutton.addEventListener("click", hideSideBar);
+
+function hideSideBar() {
+  toogle = false;
+  header.style.left = "-768px";
+  document.body.style.overflow = "unset";
+}
+
+function showSideBar() {
+  toogle = true;
+  header.style.left = "0px";
+  document.body.style.overflow = "hidden";
+}
+
+// Sticky Navbar
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  if (document.documentElement.scrollTop > 0) {
+    Desktopheader.classList.add("shadow-md");
+    Desktopheader.classList.add("bg-secondary");
+  } else {
+    Desktopheader.classList.remove("shadow-md");
+    Desktopheader.classList.remove("bg-secondary");
+  }
+}
+
+// Testimonial Slick Slider
+
+$(".testimonial_slider").slick({
+  prevArrow: $(".slick-prev"),
+  nextArrow: $(".slick-next"),
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  infinite: true,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+  ],
+});
--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -8,22 +8,30 @@ tailwind.config = {
       xl: "1280px",
       "2xl": "1536px",
     },
-
     extend: {
       animation: {
         "spin-slow": "spin 10s linear infinite",
       },
       fontSize: {
-        sm: "16px",
-        lg: "24px",
-        xl: "32px",
-        "4xl": "48px",
+        xs: "0.75rem", // 12px
+        sm: "0.875rem", // 14px
+        base: "1rem", // 16px
+        lg: "1.125rem", // 18px
+        xl: "1.25rem", // 20px
+        "2xl": "1.5rem", // 24px
+        "3xl": "1.875rem", // 30px
+        "4xl": "2.25rem", // 36px
+        "5xl": "3rem", // 48px
       },
       colors: {
-        primary: "#00CCBB",
-        secondary: "#FFFbE3",
-        black: "#1C1412",
+        primary: "#f16159",
+        secondary: "#d3e3e2",
+        lightsecondary: "#dce8e8",
       },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
     },
   },
 };
